4510108f8dc899430d8c71292064feabc586dc31
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-24 15:38:20 -0700

    all the user crud w/ validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,44 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User was successfully created"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+     if @user.update(user_params)
+       flash[:notice] = "User was successfully updated"
+       redirect_to user_path(@user)
+     else
+       render :edit
+     end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:notice] = "User was successfully destroyed"
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 
+  validates :email, presence: { message: "Email is too short (minimum is 5 characters)" }, length: {minimum: 5}
+
 end
